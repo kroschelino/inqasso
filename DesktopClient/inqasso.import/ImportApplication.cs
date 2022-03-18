278e7dbd0b7df9e97c2fe196a2d12ae46441750e
kroschel <stefan.kroschel.contractor@draeger.com>
2022-03-18 16:34:12 +0100

    Add basic console application

---
+++ DesktopClient/inqasso.import/ImportApplication.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System;
+using System.Reflection;
+using ConsoLovers.ConsoleToolkit.Core;
+using ConsoLovers.ConsoleToolkit.Core.CommandLineArguments;
+
+namespace Inqasso.Import
+{
+    internal class ImportApplication : ConsoleApplication<ImportArguments>
+    {
+        public ImportApplication(ICommandLineEngine commandLineEngine) : base(commandLineEngine)
+        {
+
+        }
+
+        public override void RunWith(ImportArguments arguments)
+        {
+            if (arguments.Version)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                Console.WriteLine($"{versionInfo.ProductMajorPart}.{versionInfo.ProductMinorPart}.0");
+            }
+        }
+    }
+
+    internal class ImportArguments
+    {
+        [Option("Version", "v")]
+        [HelpText("Shows version information")]
+        public bool Version { get; set; }
+
+        [Command("Help", "h", "?")]
+        [HelpText("Displays the help you are watching at the moment.", "None")]
+        public HelpCommand Help { get; set; }
+    }
+}
+
+
--- DesktopClient/inqasso.import/Program.cs
+++ DesktopClient/inqasso.import/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoLovers.ConsoleToolkit.Core;
+
+namespace Inqasso.Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleApplicationManager.For<ImportApplication>().SetWindowTitle("Inqasso Import").Run(args);
+        }
+
+    }
+}
